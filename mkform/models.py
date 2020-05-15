9d6ef8188d0e2379889778109c443bda3c2539b5
git-westriver <westriver.naoki20000316@gmail.com>
2020-05-15 23:15:42 +0900

    copied from https://narito.ninja/blog/detail/95/

--- mkform/models.py
+++ mkform/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+
+class Diary(models.Model):
+    text = models.TextField('日記')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Comment(models.Model):
+    text = models.CharField('コメント', max_length=300)
+    target = models.ForeignKey(Diary, on_delete=models.CASCADE, verbose_name='紐づく日記')
+    created_at = models.DateTimeField(auto_now_add=True)
