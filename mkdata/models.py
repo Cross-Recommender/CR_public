338157a014a003fd08e3b29f0570ca30ae7bacc3
git-westriver <westriver.naoki20000316@gmail.com>
2020-05-17 13:16:53 +0900

    Made assessment value float, not integer

--- mkdata/models.py
+++ mkdata/models.py
@@ -2,15 +2,15 @@ from django.db import models
 
 # Create your models here.
 
-class works(models.Model):
-    name = models.CharFielf(max_length = 200)
-    like_average = model.IntegerField(default = 0)
-    joy = model.IntegerField(default = 0)
-    anger = model.IntegerField(default = 0)
-    sadness = model.IntegerField(default=0)
-    fun = model.IntegerField(default = 0)
-    tech_constitution = model.IntegerField(default = 0)
-    tech_story = model.IntegerField(default = 0)
-    tech_character = model.IntegerField(default = 0)
-    tech_speech = model.IntegerField(default = 0)
-    tech_picture = model.IntegerField(default = 0)
+class work(models.Model):
+    name = models.CharField(max_length = 200)
+    like_average = models.FloatField(default = 0)
+    joy = models.FloatField(default = 0)
+    anger = models.FloatField(default = 0)
+    sadness = models.FloatField(default = 0)
+    fun = models.FloatField(default = 0)
+    tech_constitution = models.FloatField(default = 0)
+    tech_story = models.FloatField(default = 0)
+    tech_character = models.FloatField(default = 0)
+    tech_speech = models.FloatField(default = 0)
+    tech_picture = models.FloatField(default = 0)
